d7b0a87ada5c681c8cbd0bd38b20c0cad41f82c5
cheche482010 <cheche482010@gmail.com>
2023-03-26 22:07:07 -0400

    LOGS LISTO

--- componentes/Errores.php
+++ componentes/Errores.php
@@ -2,24 +2,23 @@
 
 class Errores
 {
-    private $ruta = '/componentes/logs/';
-    
     public function __construct()
     {
-        set_error_handler(array($this, 'manejo_errores'));
+        set_error_handler(array($this, 'Manejo_Errores'));
+        set_exception_handler(array($this, 'Manejo_Excepciones'));
     }
     
-    public function manejo_errores($errno, $errstr, $errfile, $errline)
+    public function Manejo_Errores($errno, $errstr, $errfile, $errline)
     {
         switch ($errno) {
             case E_USER_ERROR:
-                $tipoError = 'modelo-error';
+                $tipoError = 'modelo';
                 break;
             case E_USER_WARNING:
-                $tipoError = 'controlador-warning';
+                $tipoError = 'controlador';
                 break;
             case E_USER_NOTICE:
-                $tipoError = 'sistema-notice';
+                $tipoError = 'sistema';
                 break;
             default:
                 $tipoError = 'general';
@@ -27,12 +26,26 @@ class Errores
         }
         
         $log = date('[Y-m-d H:i:s]') . " Error $errno: $errstr en $errfile:$errline\n";
-        error_log($log, 3, $this->ruta . $tipoError . '.log');
+        error_log($log, 3, "componentes/logs/". $tipoError . '.log');
     }
     
+    public function Manejo_Excepciones($exception)
+    {
+        $log = date('[Y-m-d H:i:s]') . " Excepción: " . $exception->getMessage() . " en " . $exception->getFile() . ":" . $exception->getLine() . "\n";
+        error_log($log, 3, "componentes/logs/excepciones.log");
+    }
+    
+    public static function Personalizado($mensaje)
+    {
+
+        $log = date('[Y-m-d H:i:s]') . " Error personalizado: $mensaje\n";
+        error_log($log, 3, "componentes/logs/personalizado.log");
+    }
+
     public static function Capturar()
     {
         return new self();
     }
 }
+
 
