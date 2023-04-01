22a0420202640616110bb7e3611bfeb1e355a503
cheche482010 <cheche482010@gmail.com>
2023-04-01 13:14:41 -0400

    multi carga

--- app/Controlador.php
+++ app/Controlador.php
@@ -27,7 +27,7 @@ class Controlador
 
         $class = new Clases($nombre_modelo);
         if ($class->validar()) {
-            $this->modelo          = $class->instanciar();
+            $this->modelo = $class->instanciar();
         } else {
             Errores::Capturar()->Personalizado('No se pudo cargar el modelo');
         }
@@ -68,6 +68,69 @@ class Controlador
             $this->modelo->entidad = $class->instanciar();
         } else {
             Errores::Capturar()->Personalizado('No se pudo cargar la entidad');
+        }
+    }
+
+    public function Cargar($tipo, $nombre)
+    {
+        $nombre  = ucfirst($nombre);
+        $archivo = strtolower($nombre);
+        switch ($tipo) {
+            case 'modelo':
+                $nombre_clase = $nombre . '_Modelo';
+                $direccion    = 'modelo/' . $archivo . '_class.php';
+                break;
+            case 'entidad':
+                $nombre_clase = $nombre . '_Entidad';
+                $direccion    = 'modelo/entidades/' . $archivo . '.php';
+                break;
+            case 'controlador':
+                $nombre_clase = $nombre;
+                $direccion    = 'controlador/' . $archivo . '_controlador.php';
+                break;
+            case 'propiedad':
+                $nombre_clase = $nombre . '_Propiedad';
+                $direccion    = 'controlador/propiedades/' . $archivo . '.php';
+                break;
+            case 'validacion':
+                $nombre_clase = $nombre . '_Validacion';
+                $direccion    = 'componentes/validacion/' . $archivo . '_validacion.php';
+                break;
+
+            default:
+                Errores::Capturar()->Personalizado('Tipo de carga no válido');
+                return;
+        }
+
+        // Autocarga de clases
+        spl_autoload_register(function ($nombre_clase) use ($direccion) {
+            if (file_exists($direccion)) {
+                require_once $direccion;
+            }
+        });
+
+        $class = new Clases($nombre_clase);
+        if ($class->validar()) {
+            switch ($tipo) {
+                case 'modelo':
+                    $this->modelo = $class->instanciar();
+                    break;
+                case 'entidad':
+                    $this->modelo->entidad = $class->instanciar();
+                    break;
+                case 'controlador':
+                    $this->controlador = $class->instanciar();
+                    break;
+                case 'propiedad':
+                    $this->controlador->propiedad = $class->instanciar();
+                    break;
+                case 'validacion':
+                    $this->controlador->validacion = $class->instanciar();
+                    break;
+
+            }
+        } else {
+            Errores::Capturar()->Personalizado('No se pudo cargar el ' . $tipo);
         }
     }
 
