0fc4d64593da560b4f114da8f2a9843880a724a9
cheche482010 <cheche482010@gmail.com>
2023-04-14 13:05:10 -0400

    ajuste ejemplo modelo

--- componentes/Direcciones.php
+++ componentes/Direcciones.php
@@ -4,9 +4,9 @@ class Direcciones
     public static function Seguridad($string, $accion = null)
     {
         // Advanced Encryption Standard cipher-block chaining
-        $metodo        = "AES-256-CBC"; //El método de cifrado //clave simétrica de 256 bits
-        $llave = openssl_digest("key", 'whirlpool', true); //genera un hash usando el método dado y devuelve codificada (512 bits)
-        $iv    = substr(hash("whirlpool", $llave), 0, 16); // ciframos el vector de inicialización y acortamos con substr a 16
+        $metodo = "AES-256-CBC"; //El método de cifrado //clave simétrica de 256 bits
+        $llave  = openssl_digest("key", 'whirlpool', true); //genera un hash usando el método dado y devuelve codificada (512 bits)
+        $iv     = substr(hash("whirlpool", $llave), 0, 16); // ciframos el vector de inicialización y acortamos con substr a 16
 
         if ($accion == 'codificar') {
             $salida = openssl_encrypt($string, $metodo, $llave, 0, $iv); // ciframos la direccion obtenida con el metodo openssl_encrypt
@@ -16,19 +16,19 @@ class Direcciones
             $salida = openssl_decrypt($string, $metodo, $llave, 0, $iv);
         }
         return $salida;
-        unset($metodo,$llave,$iv,$accion,$sting,$salida);
+        unset($metodo, $llave, $iv, $accion, $sting, $salida);
     }
 
-    public static function URL() {
+    public static function URL()
+    {
         $protocol = empty($_SERVER['HTTPS']) ? 'http://' : 'https://';
-        $domain = $_SERVER['HTTP_HOST'];
-        $root = str_replace('/index.php', '', $_SERVER['SCRIPT_NAME'])."/";
-        $url = $protocol . $domain . $root;
+        $domain   = $_SERVER['HTTP_HOST'];
+        $root     = str_replace('/index.php', '', $_SERVER['SCRIPT_NAME']) . "/";
+        $url      = $protocol . $domain . $root;
         unset($protocol, $domain, $root);
         return $url;
     }
 
-   
     public static function _000_($value)
     {
         return self::Seguridad($value, 'codificar');
--- modelo/ejemplo_class.php
+++ modelo/ejemplo_class.php
@@ -5,20 +5,42 @@ class Ejemplo_Modelo extends Modelo
     #Public: acceso sin restricción.
     #Private:Solo puede ser accesado por la clase que lo define.
 
-    private $SQL; #nombre de la sentencia SQL que se ejecutara en el modelo
-    private $sentencia; #sentencia sql que se ejecutara
+    private $SQL;
+    private $datos;
+    private $opciones;
+    private $sentencia;
+    private $configuracion;
+    private $opciones_predeterminadas;
 
     public $resultado; #resultado de consultas de la bd
 
     public function __construct()
-    {parent::__construct();}
+    {
+        parent::__construct();
 
-    // SETTER estaablece los datos a usar en el modelo (tipo void no retornan un valor)
-    public function _SQL_(string $SQL): self
-    {$this->SQL = $SQL;return $this;}
+        $this->opciones_predeterminadas = [
+            'forzado'     => 'MIN',
+            'transaccion' => false,
+            'tipo_valor'  => 'detallado',
+            'ultimo_id'   => false,
+        ];
 
-    public function _Datos_(array $datos): self
-    {$this->datos = $datos;return $this;}
+        $this->sentencia = $this->Sentencia();
+    }
+
+    public function Configurar(array $configuracion): self
+    {
+        $this->configuracion = $configuracion;
+        if (isset($this->configuracion['SQL'])) {$this->SQL = $this->configuracion['SQL'];}
+
+        if (isset($this->configuracion['datos'])) {$this->datos = $this->configuracion['datos'];}
+
+        if (isset($this->configuracion['opciones'])) {
+            $this->opciones = array_merge($this->opciones_predeterminadas, $this->configuracion['opciones']);
+        }
+
+        return $this;
+    }
 
     private function Sentencia():  ? string
     {
@@ -28,25 +50,18 @@ class Ejemplo_Modelo extends Modelo
 
     public function Administrar(string $tipo = 'simple', array $opciones = []) : mixed
     {
-        // Definir opciones predeterminadas
-        $this->opciones_predeterminadas = [
-            'forzado'     => 'MIN',
-            'transaccion' => false,
-            'tipo_valor'  => 'detallado',
-            'ultimo_id'   => false,
-        ];
-
-        // Fusionar opciones predeterminadas con opciones proporcionadas
-        $this->opciones = array_merge($this->opciones_predeterminadas, $opciones);
-
-        $this->sentencia = $this->Sentencia();
         try {
-            $this->resultado = ($tipo === 'detallado') ? $this->Ejecutar_Detallado($this->sentencia, $this->datos, $this->opciones['forzado'], $this->opciones['transaccion'], $this->opciones['tipo_valor'], $this->opciones['ultimo_id']) : $this->Ejecutar_Simple($this->sentencia, $this->datos);
+            $this->resultado = ($tipo === 'detallado') ? $this->($this->sentencia, $this->datos, $this->opciones['forzado'], $this->opciones['transaccion'], $this->opciones['tipo_valor'], $this->opciones['ultimo_id']) : $this->($this->sentencia, $this->datos);
             $this->Desconectar();
             return $this->resultado;
         } catch (PDOException $e) {
             Errores::Capturar()->Manejo_Excepciones($e);
         }
+    }
+
+    public function SQL_01(): string
+    {
+        return $this->CRUD('consultar')->tabla('tabla')->orden('columna')->SQL();
     }
 
 }
